774f467d6b3c6789c5d81721fe2195132e406907
AjayGupta1358 <ajayg3731@gmail.com>
2025-11-20 00:51:59 +0530

    this

---
+++ Desktop/ajay/hy.c
@@ -0,0 +1,7 @@
+#include<stdint.h>
+int main()
+{
+    printf("jai shree ram");
+    printf("hello");
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"jai shree ram";
+    
+    return 0;
+}
